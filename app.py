e96c3ed092fa7a57fb17393ef75ef6a3ed18839d
M Prashanthi <151544236+Prashanthi301@users.noreply.github.com>
2025-02-11 11:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -203,8 +203,8 @@ if not reviews.empty:
    reviews["review_statements"] = reviews["review_statements"].apply(
       lambda x: truncate_text(x, 512)
    )
-   reviews = reviews["review_statements"].tolist()
-   sentiments = analyze_sentiment(review_statements)
+   reviews = reviews["reviews"].tolist()
+   sentiments = analyze_sentiment(reviews)
 
    st.subheader("Customer Sentiment Analysis")
    sentiment_df = pd.DataFrame(sentiments)
